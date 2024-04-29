24a9a6717b6968860b5049d991873e813a2183ae
candianerea <CandiaSalma1@gmail.com>
2024-04-29 16:58:37 -0300

    avance con los ejercicios

---
+++ Ejercicios html/integracionjs/script.js
@@ -0,0 +1,19 @@
+// Declarar funciones 
+
+//funcion tradicional
+//function myFunction(){ 
+//return
+//}
+
+
+
+let contador = 0;
+
+//document -> siempre los nombres de las funciones siguen la nomenclaturas de camecase
+
+function incrementaNumero() {
+  //  let number = document.getElementById("number").textContent;
+    //let number 0 document.getElementById("number").textContent;
+    contador = contado + 1;
+    document.getElementById("number").textContent = contador;
+}
