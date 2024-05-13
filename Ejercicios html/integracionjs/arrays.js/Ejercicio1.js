586ff169ed83925309e088dedaacb7ce34399379
candianerea <SalmaCandia1@gmail.com>
2024-05-13 16:52:24 -0300

    13 05 2024

---
+++ Ejercicios html/integracionjs/arrays.js/Ejercicio1.js
@@ -0,0 +1,17 @@
+const array = ["Candia Nerea", "pizza", "hamburguesa"];
+
+
+
+function procesarPedido(pedidos) {
+//codigo
+
+const clente = pedidos.shift();
+const bebidas = "sprite"
+pedidos.unshift(bebida)
+
+}
+
+consoles.log(procesadorPedido(array));
+
+while(contador <array.length) {
+    console.log(array)
